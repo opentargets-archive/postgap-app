551d382f44e1485dfd27c5506c811852e6172210
peatroot <garethpeat@gmail.com>
2018-02-09 16:56:23 +0000

    refactor: Add transformer for evidence strings

--- src/store.js
+++ src/store.js
@@ -2,6 +2,25 @@ import { createStore } from 'redux';
 
 import rawData from './raw.json';
 
+function transformEvidenceString(r) {
+    return {
+        efoId: r.disease.id,
+        efoName: r.disease.name,
+        gwasPValue: r.evidence.variant2disease.resource_score.value,
+        gwasSampleSize: r.evidence.variant2disease.gwas_sample_size,
+        gwasSnpId: r.evidence.variant2disease.lead_snp_rsid,
+        r2: parseFloat(r.unique_association_fields.r2),
+        ldSnpId: r.variant.id.split('/')[4],
+        gtex: r.evidence.gene2variant.metadata.funcgen.gtex_score,
+        pchic: r.evidence.gene2variant.metadata.funcgen.pchic_score,
+        dhs: r.evidence.gene2variant.metadata.funcgen.dhs_score,
+        fantom5: r.evidence.gene2variant.metadata.funcgen.fantom5_score,
+        vep: r.evidence.gene2variant.metadata.funcgen.vep_score,
+        otScore: r.evidence.gene2variant.metadata.funcgen.ot_g2v_score,
+        geneName: r.target.target_name,
+    }
+}
+
 // TODO: Subdivide state and reducers and async load
 //       initialState url: https://mk-loci-dot-open-targets-eu-dev.appspot.com/v3/platform/public/evidence/filter?chromosome=1&begin=109167885&end=109612066&size=10&datasource=gwas_catalog&fields=unique_association_fields&fields=disease&fields=evidence&fields=variant&fields=target&fields=sourceID
 const initialState = {
@@ -10,7 +29,7 @@ const initialState = {
         start: 109167885,
         end: 109612066,
     },
-    rows: rawData.data,
+    rows: rawData.data.map(transformEvidenceString),
 }
 
 function reducer (state=initialState, action) {
