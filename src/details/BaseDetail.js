62d8f9356cc219a486f72f2f9c52aff19461c425
peatroot <garethpeat@gmail.com>
2018-02-14 13:11:13 +0000

    style: Test usage of husky precommit hook to run prettier

--- src/details/BaseDetail.js
+++ src/details/BaseDetail.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Card, Row, Col, Icon } from 'antd';
 
-const BaseDetail = (props) => {
-    return <Card bodyStyle={{padding: 10}}>
-        <Row>
-            <Col span={16}>
-                <span style={{fontWeight: 100, fontStyle: 'italic'}}>{props.type} </span>
-                <span>{props.title}</span>
-            </Col>
-            <Col span={8} align='end'>
-                <Icon type="close" />
-            </Col>
-        </Row>
-        <hr />
-        {props.children}
+const BaseDetail = props => {
+  return (
+    <Card bodyStyle={{ padding: 10 }}>
+      <Row>
+        <Col span={16}>
+          <span style={{ fontWeight: 100, fontStyle: 'italic' }}>
+            {props.type}{' '}
+          </span>
+          <span>{props.title}</span>
+        </Col>
+        <Col span={8} align="end">
+          <Icon type="close" />
+        </Col>
+      </Row>
+      <hr />
+      {props.children}
     </Card>
-}
+  );
+};
 
 export default BaseDetail;
