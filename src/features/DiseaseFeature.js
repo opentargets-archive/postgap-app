d5ce85ca41b72c7637f2196ad6b5d48965195992
peatroot <garethpeat@gmail.com>
2018-02-15 10:04:12 +0000

    refactor: Use react-svg-text to wrap disease labels

--- src/features/DiseaseFeature.js
+++ src/features/DiseaseFeature.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import Text from 'react-svg-text';
 
 const PADDING = 0.2; // 20%
-const calculateDiseaseScaleRange = (width) => ([width * PADDING, width * (1 - PADDING)])
+const calculateDiseaseScaleRange = width => [
+  width * PADDING,
+  width * (1 - PADDING)
+];
 
 const DiseaseFeature = ({ scale, data, diseaseScale, width }) => {
-    const { y } = scale;
-    diseaseScale.range(calculateDiseaseScaleRange(width)); // TODO: refactor to set range in better location
-    return (
-        <g>
-            <circle cx={diseaseScale(data.id)} cy={y(0.6)} r={4} style={{ stroke: 'blue', strokeWidth: 2, fill: 'lightgrey' }} />
-            <text x={diseaseScale(data.id)} y={y(0.6)} dy={15} style={{ alignmentBaseline: 'middle', textAnchor: 'middle' }}>
-                {data.name}
-            </text>
-        </g>
-    )
-}
+  const { y } = scale;
+  diseaseScale.range(calculateDiseaseScaleRange(width)); // TODO: refactor to set range in better location
+  return (
+    <g>
+      <circle
+        cx={diseaseScale(data.id)}
+        cy={y(0.7)}
+        r={4}
+        style={{ stroke: 'blue', strokeWidth: 2, fill: 'lightgrey' }}
+      />
+      <Text
+        x={diseaseScale(data.id)}
+        y={y(0.7) + 10}
+        width={150}
+        textAnchor="middle"
+        verticalAnchor="start"
+        style={{ fontSize: '12px' }}
+      >
+        {data.name}
+      </Text>
+    </g>
+  );
+};
 
 export default DiseaseFeature;
