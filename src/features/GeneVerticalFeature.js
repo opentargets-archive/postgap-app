6ccb201800dcf754bd4b9936ca28e862874f2cea
peatroot <garethpeat@gmail.com>
2018-02-21 14:10:07 +0000

    fix: Use TSS for GeneVerticalFeature

--- src/features/GeneVerticalFeature.js
+++ src/features/GeneVerticalFeature.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const GeneVerticalFeature = ({ scale, data, slotOffset, slotHeight, trackHeight }) => {
-    const { x } = scale;
+const GeneVerticalFeature = ({
+  scale,
+  data,
+  slotOffset,
+  slotHeight,
+  trackHeight
+}) => {
+  const { x } = scale;
 
-    return <line 
-        x1={x(data.canonicalTranscript.start)} 
-        y1={slotOffset} 
-        x2={x(data.canonicalTranscript.start)} 
-        y2={trackHeight} 
-        style={{ stroke: 'grey', strokeWidth: 1 }}
+  return (
+    <line
+      x1={x(data.canonicalTranscript.tss)}
+      y1={slotOffset}
+      x2={x(data.canonicalTranscript.tss)}
+      y2={trackHeight}
+      style={{ stroke: 'grey', strokeWidth: 1 }}
     />
-}
+  );
+};
 
 export default GeneVerticalFeature;
