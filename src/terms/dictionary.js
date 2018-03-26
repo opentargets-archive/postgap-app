b3a7b3df38acaf580875eba877a46a4a6e5e4563
peatroot <garethpeat@gmail.com>
2018-03-26 15:39:50 +0100

    fix: Fixes #1. Update terminology definitions (ran functional genomics ones past William N and Danie

--- src/terms/dictionary.js
+++ src/terms/dictionary.js
@@ -2,24 +2,27 @@
 
 const DICTIONARY = {
   otscore:
-    'The Open Targets G2V score is an aggregate score based upon VEP, GTEx, PCHiC, DHS, Fantom5 and Nearest fields.',
+    'The Open Targets G2V score is an aggregation of the VEP, GTEx, PCHiC, DHS, Fantom5 and Nearest subscores. Within [0, 1].',
   vep:
-    'The Variant Effect Predictor consequence expressed on a scale from 0 to 4.',
+    'The Variant Effect Predictor (VEP) score is taken as the consequence expressed on a scale from 0 to 4.',
   gtex:
-    'The max(1 - p-value) across all tissues for the eQTL between the target and variant (source: GTEx).',
+    'The GTEx score is taken as max(1 - p-value) across all tissues for the eQTL between the given target and variant. Within [0, 1].',
   pchic:
-    'The max normalized Promoter Capture Hi-C score (given by CHiCAGO) for linked sites containing variant and target (across all tissues).',
+    'The PCHiC score is taken as max normalized Promoter Capture Hi-C score (CHiCAGO output) for linked sites containing variant and target (across all tissues). Within [0, 1].',
   dhs:
-    '(1 - false discovery rate) for linked sites (via correlated DNase hypersensitivity) containing variant and target (source: DHS).',
+    'The DHS score is taken as (1 - false discovery rate) for linked sites (via correlated DNase hypersensitivity) containing variant and target. Within [0, 1].',
   fantom5:
-    '(1 - false discovery rate) for linked sites (miRNAs and their promoters) containing variant and target (source: Fantom5).',
-  r2: 'The linkage disequilibrium between the lead variant and the variant.',
-  gwaspvalue: 'The reported p-value from GWAS catalog.',
-  gwassamplesize: 'The reported sample size from GWAS catalog.',
-  variants: 'Single nucleotide polymorphisms.',
+    'The Fantom5 score is taken as (1 - false discovery rate) for linked sites (miRNAs and their promoters) containing variant and target. Within [0, 1].',
+  r2:
+    'The linkage disequilibrium (r2) between the lead variant and the variant.',
+  gwaspvalue:
+    'The reported p-value from GWAS catalog between the lead variant and disease.',
+  gwassamplesize:
+    'The reported sample size from GWAS catalog between the lead variant and disease.',
+  variants: 'Variants from Ensembl with at least one association.',
   leadvariants:
-    'Single nucleotide polymorphisms identified as being associated with a disease through a GWAS.',
-  genes: 'Genes from Ensembl.',
+    'Variants identified as being associated with a disease directly through a GWAS.',
+  genes: 'Genes from Ensembl with at least one association.',
   diseases:
     'Diseases identified as being associated with a lead variant through a GWAS.',
 };
