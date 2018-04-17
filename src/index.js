772d66e5ab92ed527e9f2561a082cbcdd39c616b
peatroot <garethpeat@gmail.com>
2018-04-17 10:11:25 +0100

    refactor: Remove redux store usage

--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import store from './redux/store';
-import { Provider } from 'react-redux';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+    uri: 'http://localhost:4000/graphql',
 });
 
 render(
-  <ApolloProvider client={client}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </ApolloProvider>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
 );
 registerServiceWorker();
