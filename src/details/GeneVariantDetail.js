c27f100a1db5474ca61d1e2ef721154195fc3926
peatroot <garethpeat@gmail.com>
2018-04-17 09:38:36 +0100

    style: Apply eslint to GeneVariantDetail

--- src/details/GeneVariantDetail.js
+++ src/details/GeneVariantDetail.js
@@ -6,85 +6,85 @@ import { LinksGene, LinksVariant } from '../links';
 import { renderNonZeroField } from '../stringFormatters';
 
 const GeneVariantDetail = ({ geneVariant, closeHandler }) => {
-  const d = geneVariant;
-  const tableData = [
-    {
-      key: 'otG2VScore',
-      label: 'G2V Score',
-      value: d.otG2VScore,
-    },
-    {
-      key: 'vep',
-      label: 'VEP',
-      value: d.vep,
-    },
-    {
-      key: 'gtex',
-      label: 'GTEx',
-      value: d.gtex,
-    },
-    {
-      key: 'pchic',
-      label: 'PCHiC',
-      value: d.pchic,
-    },
-    {
-      key: 'dhs',
-      label: 'DHS',
-      value: d.dhs,
-    },
-    {
-      key: 'fantom5',
-      label: 'Fantom5',
-      value: d.fantom5,
-    },
-  ];
-  const tableColumns = [
-    {
-      key: 'label',
-      title: 'Label',
-      dataIndex: 'label',
-      width: 100,
-      render: (text, row) => (
-        <DictionaryHelpTerm term={row.key} label={row.label} />
-      ),
-    },
-    {
-      key: 'value',
-      title: 'Value',
-      dataIndex: 'value',
-      width: 100,
-      render: renderNonZeroField,
-    },
-  ];
-  return (
-    <BaseDetail
-      type={'Gene - Variant'}
-      title={
-        <React.Fragment>
-          <LinksGene
-            geneName={geneVariant.geneSymbol}
-            geneId={geneVariant.geneId}
-          >
-            {geneVariant.geneSymbol}
-          </LinksGene>
-          {' - '}
-          <LinksVariant variantId={geneVariant.variantId}>
-            {geneVariant.variantId}
-          </LinksVariant>
-        </React.Fragment>
-      }
-      closeHandler={closeHandler}
-    >
-      <Table
-        dataSource={tableData}
-        columns={tableColumns}
-        size="small"
-        pagination={false}
-        showHeader={false}
-      />
-    </BaseDetail>
-  );
+    const d = geneVariant;
+    const tableData = [
+        {
+            key: 'otG2VScore',
+            label: 'G2V Score',
+            value: d.otG2VScore,
+        },
+        {
+            key: 'vep',
+            label: 'VEP',
+            value: d.vep,
+        },
+        {
+            key: 'gtex',
+            label: 'GTEx',
+            value: d.gtex,
+        },
+        {
+            key: 'pchic',
+            label: 'PCHiC',
+            value: d.pchic,
+        },
+        {
+            key: 'dhs',
+            label: 'DHS',
+            value: d.dhs,
+        },
+        {
+            key: 'fantom5',
+            label: 'Fantom5',
+            value: d.fantom5,
+        },
+    ];
+    const tableColumns = [
+        {
+            key: 'label',
+            title: 'Label',
+            dataIndex: 'label',
+            width: 100,
+            render: (text, row) => (
+                <DictionaryHelpTerm term={row.key} label={row.label} />
+            ),
+        },
+        {
+            key: 'value',
+            title: 'Value',
+            dataIndex: 'value',
+            width: 100,
+            render: renderNonZeroField,
+        },
+    ];
+    return (
+        <BaseDetail
+            type={'Gene - Variant'}
+            title={
+                <React.Fragment>
+                    <LinksGene
+                        geneName={geneVariant.geneSymbol}
+                        geneId={geneVariant.geneId}
+                    >
+                        {geneVariant.geneSymbol}
+                    </LinksGene>
+                    {' - '}
+                    <LinksVariant variantId={geneVariant.variantId}>
+                        {geneVariant.variantId}
+                    </LinksVariant>
+                </React.Fragment>
+            }
+            closeHandler={closeHandler}
+        >
+            <Table
+                dataSource={tableData}
+                columns={tableColumns}
+                size="small"
+                pagination={false}
+                showHeader={false}
+            />
+        </BaseDetail>
+    );
 };
 
 export default GeneVariantDetail;
