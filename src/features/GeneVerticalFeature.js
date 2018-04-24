272eb9ce7306f39dd7b22b0a34be9372181e57d3
peatroot <garethpeat@gmail.com>
2018-04-24 09:39:55 +0100

    Fix indentation

--- src/features/GeneVerticalFeature.js
+++ src/features/GeneVerticalFeature.js
@@ -3,27 +3,27 @@ import React from 'react';
 import { colors } from '../theme';
 
 const GeneVerticalFeature = ({
-  scale,
-  data,
-  slotOffset,
-  slotHeight,
-  trackHeight,
-  highlight,
-  dimNonHighlighted,
+    scale,
+    data,
+    slotOffset,
+    slotHeight,
+    trackHeight,
+    highlight,
+    dimNonHighlighted,
 }) => {
-  const { x } = scale;
-  const geneColor = highlight
-    ? colors.secondary
-    : dimNonHighlighted ? 'lightgrey' : 'grey';
-  return (
-    <line
-      x1={x(data.canonicalTranscript.tss)}
-      y1={slotOffset}
-      x2={x(data.canonicalTranscript.tss)}
-      y2={trackHeight}
-      style={{ stroke: geneColor, strokeWidth: 1 }}
-    />
-  );
+    const { x } = scale;
+    const geneColor = highlight
+        ? colors.secondary
+        : dimNonHighlighted ? 'lightgrey' : 'grey';
+    return (
+        <line
+            x1={x(data.canonicalTranscript.tss)}
+            y1={slotOffset}
+            x2={x(data.canonicalTranscript.tss)}
+            y2={trackHeight}
+            style={{ stroke: geneColor, strokeWidth: 1 }}
+        />
+    );
 };
 
 export default GeneVerticalFeature;
