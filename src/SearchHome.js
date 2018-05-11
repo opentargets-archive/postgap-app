4cf424ce05e9d4a4cb68348fcb1c4713b336c999
peatroot <garethpeat@gmail.com>
2018-05-11 11:10:37 +0100

    Use search endpoint

--- src/SearchHome.js
+++ src/SearchHome.js
@@ -1,107 +1,135 @@
 import React from 'react';
+import { withApollo } from 'react-apollo';
+import gql from 'graphql-tag';
 import { withRouter } from 'react-router-dom';
 import { Select, Spin } from 'antd';
 import { debounce } from 'lodash';
 
-import { otApi, ensemblApi } from './redux/sagas';
+import { otApi } from './redux/sagas';
 import { colors } from './theme';
 
 const Option = Select.Option;
 
+const SEARCH_QUERY = gql`
+    query SearchQuery($queryString: String) {
+        search(queryString: $queryString) {
+            id
+            name
+            type
+        }
+    }
+`;
+
 class SearchHome extends React.Component {
-  constructor(props) {
-    super(props);
-    this.lastFetchId = 0;
-    this.fetchSearchResults = debounce(this.fetchSearchResults, 800);
-  }
-  state = {
-    data: [],
-    value: [],
-    fetching: false,
-  };
-  fetchSearchResults = value => {
-    // only query if at least three characters have been typed
-    if (value.length > 2) {
-      this.lastFetchId += 1;
-      this.setState({ data: [], options: [], fetching: true });
-      Promise.all([
-        otApi.fetchSearch(value),
-        ensemblApi.fetchSearch(value),
-      ]).then(([otData, ensemblData]) => {
-        const data = [...ensemblData, ...otData];
-        this.setState({ data, options: data, fetching: false });
-      });
+    constructor(props) {
+        super(props);
+        this.lastFetchId = 0;
+        this.fetchSearchResults = debounce(this.fetchSearchResults, 800);
     }
-  };
-  handleChange = value => {
-    this.setState({
-      value: [],
-      data: [],
-      fetching: false,
-    });
+    state = {
+        data: [],
+        value: [],
+        fetching: false,
+    };
+    fetchSearchResults = value => {
+        const { client } = this.props;
+        // only query if at least three characters have been typed
+        if (value.length > 2) {
+            // reset options
+            this.setState({ data: [], options: [], fetching: true });
 
-    // value is an array of the selected items, but contains only key, label pairs
-    const keys = value.map(entry => entry.key);
+            // call for results
+            Promise.all([
+                otApi.fetchSearch(value),
+                client
+                    .query({
+                        query: SEARCH_QUERY,
+                        variables: { queryString: value },
+                    })
+                    .then(response => {
+                        if (response.data && response.data.search) {
+                            return response.data.search;
+                        } else {
+                            return [];
+                        }
+                    }),
+            ]).then(([otData, gqlData]) => {
+                const data = [...gqlData, ...otData];
 
-    // only allow one entry before redirect (for now)
-    if (keys.length === 1) {
-      const key = keys[0];
-      const item = this.state.options.find(item => item.id === key);
-      switch (item.type) {
-        case 'target':
-          this.props.history.push({
-            pathname: `/gene/${item.id}`,
-          });
-          break;
-        case 'disease':
-          this.props.history.push({
-            pathname: `/disease/${item.id}`,
-          });
-          break;
-        case 'variant':
-          this.props.history.push({
-            pathname: `/variant/${item.id}`,
-          });
-          break;
-        default:
-          throw Error('Unexpected item type');
-      }
-    }
-  };
-  render() {
-    const { fetching, data, value } = this.state;
-    return (
-      <Select
-        mode="multiple"
-        labelInValue
-        value={value}
-        placeholder="Search for a gene, disease or variant..."
-        notFoundContent={fetching ? <Spin size="small" /> : null}
-        filterOption={false}
-        onSearch={this.fetchSearchResults}
-        onChange={this.handleChange}
-        style={{ width: '100%', maxWidth: 400 }}
-      >
-        {data.map(d => (
-          <Option key={d.id}>
-            <span
-              style={{
-                fontStyle: 'italic',
-                fontWeight: 100,
-                fontSize: '0.7em',
-                color: colors.primary,
-              }}
+                // set options
+                this.setState({ data, options: data, fetching: false });
+            });
+        }
+    };
+    handleChange = value => {
+        this.setState({
+            value: [],
+            data: [],
+            fetching: false,
+        });
+
+        // value is an array of the selected items, but contains only key, label pairs
+        const keys = value.map(entry => entry.key);
+
+        // only allow one entry before redirect (for now)
+        if (keys.length === 1) {
+            const key = keys[0];
+            const item = this.state.options.find(item => item.id === key);
+            switch (item.type) {
+                case 'target':
+                    this.props.history.push({
+                        pathname: `/gene/${item.id}`,
+                    });
+                    break;
+                case 'disease':
+                    this.props.history.push({
+                        pathname: `/disease/${item.id}`,
+                    });
+                    break;
+                case 'variant':
+                    this.props.history.push({
+                        pathname: `/variant/${item.id}`,
+                    });
+                    break;
+                default:
+                    throw Error('Unexpected item type');
+            }
+        }
+    };
+    render() {
+        const { fetching, data, value } = this.state;
+        return (
+            <Select
+                mode="multiple"
+                labelInValue
+                value={value}
+                placeholder="Search for a gene, disease or variant..."
+                notFoundContent={fetching ? <Spin size="small" /> : null}
+                filterOption={false}
+                onSearch={this.fetchSearchResults}
+                onChange={this.handleChange}
+                style={{ width: '100%', maxWidth: 400 }}
             >
-              {d.type}{' '}
-            </span>
-            <span style={{ color: colors.grey }}>{d.name}</span>
-          </Option>
-        ))}
-      </Select>
-    );
-  }
+                {data.map(d => (
+                    <Option key={d.id}>
+                        <span
+                            style={{
+                                fontStyle: 'italic',
+                                fontWeight: 100,
+                                fontSize: '0.7em',
+                                color: colors.primary,
+                            }}
+                        >
+                            {d.type}{' '}
+                        </span>
+                        <span style={{ color: colors.grey }}>{d.name}</span>
+                    </Option>
+                ))}
+            </Select>
+        );
+    }
 }
 
-SearchHome = withRouter(SearchHome);
+SearchHome = withApollo(withRouter(SearchHome));
 
 export default SearchHome;
