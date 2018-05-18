523747b6d863ed0738c9ebcf2a7a952414be6c8f
peatroot <garethpeat@gmail.com>
2018-05-18 12:55:53 +0100

    Move withPageAnalytics to HOCs directory

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 import './App.css';
 
-import withPageAnalytics from './withPageAnalytics';
+import withPageAnalytics from './HOCs/withPageAnalytics';
 import HomePage from './pages/HomePage';
 import DiseasePage from './pages/DiseasePage';
 import GenePage from './pages/GenePage';
