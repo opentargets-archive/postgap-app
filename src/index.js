06d3573508886fd62147d68df3f2c2d4334d0b6b
peatroot <garethpeat@gmail.com>
2018-05-18 13:07:17 +0100

    Move registerServiceWorker to utils directory

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker';
+import { unregister } from './utils/registerServiceWorker';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
