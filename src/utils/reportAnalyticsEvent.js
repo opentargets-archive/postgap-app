2976e0389c882ac3daad127d2e1d273a87ab7ddc
Andrew Hercules <andrewhercules@gmail.com>
2018-07-04 15:27:09 +0100

    Change hostname to track on GA

--- src/utils/reportAnalyticsEvent.js
+++ src/utils/reportAnalyticsEvent.js
@@ -2,7 +2,7 @@ import GoogleAnalytics from 'react-ga';
 
 const reportAnalyticsEvent = options => {
     // report to analytics if production
-    if (window.location.hostname === 'postgap.opentargets.io') {
+    if (window.location.hostname === 'genetics.opentargets.io') {
         GoogleAnalytics.event(options);
     }
 };
