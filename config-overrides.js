e2730a464afec224ca131a51145c4f240f8f1121
peatroot <garethpeat@gmail.com>
2018-10-25 16:51:54 +0100

    Add react-app-rewire-graphql-tag

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,20 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast');
+const rewireGraphqlTag = require('react-app-rewire-graphql-tag');
 
 module.exports = function override(config, env) {
     config = rewireInlineImportGraphqlAst(config, env);
-    config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);  // change importing css to less
+    config = rewireGraphqlTag(config, env);
+    config = injectBabelPlugin(
+        ['import', { libraryName: 'antd', style: true }],
+        config
+    ); // change importing css to less
     config = rewireLess.withLoaderOptions({
         modifyVars: {
-            "@primary-color": "#0091eb",
-            "@slider-track-background-color": "#0091eb",
-            "@slider-handle-color": "#0091eb"
+            '@primary-color': '#0091eb',
+            '@slider-track-background-color': '#0091eb',
+            '@slider-handle-color': '#0091eb',
         },
     })(config, env);
     return config;
